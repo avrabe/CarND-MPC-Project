6ea60f35dbb18939db18baf83501e08bf7d03893
Ralf Anton Beier <ralf_beier@me.com>
2017-06-22 06:00:08 +0200

    First working version.
    Did not incorporate the latency yet.
    The parameters need some tuning.
    But the car is moving.

--- src/main.cpp
+++ src/main.cpp
@@ -125,10 +125,10 @@ int main() {
             steer_value = ret[0];
             throttle_value = ret[1];
 
-          json msgJson;
+            json msgJson;
           // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
           // Otherwise the values will be in between [-deg2rad(25), deg2rad(25] instead of [-1, 1].
-            msgJson["steering_angle"] = steer_value / deg2rad(25);
+            msgJson["steering_angle"] = steer_value / 0.46332 * -1;
           msgJson["throttle"] = throttle_value;
 
             //Display the MPC predicted trajectory
@@ -136,9 +136,9 @@ int main() {
           vector<double> mpc_y_vals;
             for (uint32_t i = 2; i < ret.size(); i++) {
                 if (i % 2) {
-                    mpc_x_vals.push_back(ret[i]);
-                } else {
                     mpc_y_vals.push_back(ret[i]);
+                } else {
+                    mpc_x_vals.push_back(ret[i]);
                 }
             }
 
@@ -149,14 +149,18 @@ int main() {
           msgJson["mpc_y"] = mpc_y_vals;
 
           //Display the waypoints/reference line
-            vector<double> next_x_vals = {0, 1, 2, 3, 4, 5};
-            vector<double> next_y_vals = {0, 1, 2, 3, 4, 5};
+            vector<double> next_x_vals;
+            vector<double> next_y_vals;
 
+            for (uint32_t i = 0; i < 40; i++) {
+                next_x_vals.push_back((double) i);
+                next_y_vals.push_back(polyeval(coeffs, i));
+            }
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Yellow line
 
-            msgJson["next_x"] = ptsx; //next_x_vals;
-            msgJson["next_y"] = ptsy; //next_y_vals;
+            msgJson["next_x"] = next_x_vals; //next_x_vals;
+            msgJson["next_y"] = next_y_vals; //next_y_vals;
 
 
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
